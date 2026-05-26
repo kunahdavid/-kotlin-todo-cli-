c3bda4924c4415573c12d140213ead3fb81bf53d
esban19 <gabrielasuarez120104@gmail.com>
2026-05-26 15:28:17 -0500

    prueba

--- -kotlin-todo-cli--main/Trivia_preguntas/BancoPreguntas.kt
+++ -kotlin-todo-cli--main/Trivia_preguntas/BancoPreguntas.kt
@@ -7,7 +7,6 @@ class BancoPreguntas {
         listaPreguntas.add(Pregunta(1, "¿Qué palabra clave define una constante en Kotlin?", listOf("var", "const val", "val", "let"), 3))
         listaPreguntas.add(Pregunta(2, "¿Cómo maneja Kotlin la seguridad contra nulos por defecto?", listOf("Permite nulos siempre", "Los tipos no son anulables por defecto", "Usa punteros", "Lanza NullPointerException"), 2))
         listaPreguntas.add(Pregunta(3, "¿Cuál es la ventaja de una Data Class?", listOf("Hereda de múltiples clases", "Genera automáticamente toString(), equals() y hashCode()", "Hace el código más rápido", "No permite funciones"), 2))
-        listaPreguntas.add(Pregunta(4, "¿Qué palabra clave se usa para definir una variable que puede cambiar?", listOf("val", "var", "fun", "class"), 2))
         listaPreguntas.add(Pregunta(5, "¿Qué palabra clave se usa para crear una función en Kotlin?", listOf("function", "def", "fun", "method"), 3))
         listaPreguntas.add(Pregunta(6, "¿Qué estructura se usa para tomar decisiones en Kotlin?", listOf("if", "print", "readLine", "listOf"), 1))
     }
